652f5164ccc629fac5672a371224990f9750f3f6
Ali Haider <alikh4837@gmail.com>
2026-07-09 14:31:41 +0500

    add fastapi-pagination dependency

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi_pagination import Page,add_pagination,paginate
 
 app = FastAPI(title="University course registration and resource management API")
+add_pagination(app)
 
 @app.get("/")
 def root():
