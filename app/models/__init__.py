52e55586ae04fcc0ddd47d61852e239f52da0313
Ali Haider <alikh4837@gmail.com>
2026-07-11 16:40:56 +0500

    refactor __init__.py; organize model imports and define __all__ for public API

---
+++ app/models/__init__.py
@@ -0,0 +1,19 @@
+from app.models.department import Department
+from app.models.instructor import Instructor
+from app.models.course import Course
+from app.models.room import Room
+from app.models.term import Term
+from app.models.section import Section
+from app.models.enrollment import Enrollment
+from app.models.student import Student
+
+__all__ = [
+    "Department",
+    "Instructor",
+    "Course",
+    "Room",
+    "Term",
+    "Section",
+    "Enrollment",
+    "Student",
+]
