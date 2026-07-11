bef1dddc1e8e17462bae0cf25655d01df74dba03
Ali Haider <alikh4837@gmail.com>
2026-07-11 18:03:41 +0500

    added department router

---
+++ app/routers/department.py
@@ -0,0 +1,59 @@
+from fastapi import APIRouter
+from app.schemas.department import DeptResponse, CreateDept,UpdateDept
+from app.services.department import Dept_Service
+from app.models.department import Department
+
+router = APIRouter(prefix="/departments", tags=["Departments"])
+
+
+@router.post("/", response_model=DeptResponse)
+async def Create_Department(data: CreateDept):
+    return Dept_Service.Create_Department(data)
+
+@router.get("/",response_model=DeptResponse)
+async def Get_Department()->DeptResponse:
+    department=Department(
+        id=1,
+        dept_name="HR",
+        dept_code=1,
+    )
+    return DeptResponse(**department.model_dump())
+
+@router.get("/list",response_model=DeptResponse)
+async def List_Department()->list[DeptResponse]:
+    departments=[
+        Department(
+        id=1,
+        dept_name="HR",
+        dept_code=1,
+    ),
+    Department(
+        id=2,
+        dept_name="HR",
+        dept_code=1,
+    )
+    ]
+    results:list[DeptResponse]=[]
+    for department in departments:
+        validated=DeptResponse(**department.model_dump())
+        results.append(validated)
+       
+    return results
+
+@router.patch("/",response_model=DeptResponse)
+async def Patch_Department()->DeptResponse:
+    department=Department(
+        id=1,
+        dept_name="HR",
+        dept_code=1,
+    )
+    return DeptResponse(**department.model_dump())
+
+@router.delete("/{id}",response_model=DeptResponse)
+async def Delete_Department(id:int)->DeptResponse:
+    department=Department(
+        id=1,
+        dept_name="HR",
+        dept_code=1,
+    )
+    return DeptResponse(**department.model_dump())
