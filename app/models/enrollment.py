a67731a53787187112b7f4429d2d856b4eabe9d7
Ali Haider <alikh4837@gmail.com>
2026-07-11 16:37:54 +0500

    refactor enrollment model; correct formatting and update relationship attributes

--- app/models/enrollment.py
+++ app/models/enrollment.py
@@ -1,16 +1,21 @@
-from sqlmodel import SQLModel, Field,Relationship
-from typing import Optional,List,TYPE_CHECKING
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional, List, TYPE_CHECKING
 
 
 if TYPE_CHECKING:
     from app.models.section import Section
     from app.models.student import Student
 
+
 class EnrollmentBase(SQLModel):
-    enrol_code:int
+    enrol_code: int
 
-class Enrollment(EnrollmentBase):
-    id:int=Field(primary_key=True)
 
-    sections:List["Section"]=Relationship(back_populates="Instructor")
-    students:List["Student"]=Relationship(back_populates="Instructor")
+class Enrollment(EnrollmentBase, table=True):
+    id: int = Field(primary_key=True)
+
+    student_id: Optional[int] = Field(default=None, foreign_key="student.id")
+    section_id: Optional[int] = Field(default=None, foreign_key="section.id")
+
+    section: Optional["Section"] = Relationship(back_populates="enrollments")
+    student: Optional["Student"] = Relationship(back_populates="enrollments")
