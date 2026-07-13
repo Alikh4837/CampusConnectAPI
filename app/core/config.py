625f8f64d55e643084d485e6b87041d461d5ea81
Ali Haider <alikh4837@gmail.com>
2026-07-13 13:19:54 +0500

    implemented password hashing and JWT token creation
    - Created __init__.py in repositories directory for package initialization.

--- app/core/config.py
+++ app/core/config.py
@@ -7,5 +7,8 @@ class Settings(BaseSettings):
     APP_VERSION: str = "1.0.0"
     DEBUG: bool = True  
 
+    SECRET_KEY:str="im_duh_secret_key"
+    ALGORITHM:str="HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES:int=10
 
 settings = Settings()
