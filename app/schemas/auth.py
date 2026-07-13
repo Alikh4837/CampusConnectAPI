86b11227fc7ce77e6bf5b1c3e909dfb99d6644c7
Ali Haider <alikh4837@gmail.com>
2026-07-13 14:18:50 +0500

    added authentication schemas

---
+++ app/schemas/auth.py
@@ -0,0 +1,15 @@
+from sqlmodel import SQLModel
+
+
+class LoginRequest(SQLModel):
+    email: str
+    password: str
+
+
+class Token(SQLModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(SQLModel):
+    username: str | None = None
