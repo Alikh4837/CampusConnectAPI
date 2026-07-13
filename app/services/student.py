348f7f976a7cf906b7ca2a43a7ecc12acbf01aa3
Ali Haider <alikh4837@gmail.com>
2026-07-13 16:18:10 +0500

    added get_by_email method to StudentService for retrieving student by email

--- app/services/student.py
+++ app/services/student.py
@@ -1,5 +1,6 @@
 from app.schemas.student import StudentResponse, CreateStudent,UpdateStudent
 from app.models.student import Student
+from app.core.security import hash_password
 
 
 class StudentService:
@@ -56,6 +57,21 @@ class StudentService:
             password="123",
             emergency_contact="123",
         )
+    def get_by_email(self,email:str)->Student|None:
+        mock_student = Student(
+            id=1,
+            name="john",
+            dob="29june2005",
+            gender="Male",
+            phone="20912",
+            email="alikh4837@gmail.com",
+            address="Johar town",
+            password=hash_password("123"),  
+            emergency_contact="123",
+        )
+        if email == mock_student.email:
+            return mock_student
+        return None
 
 
 student_service = StudentService()
